fcd8591d4c6fd2949b5333ba252a995e1c5badcb
gmpbigsun <tuncay.yildiz@live.de>
2022-06-15 19:04:27 +0200

    bugfix favorite items
    when an item was marked as favorite the order of tooltip lines changes
    this fix searches for the first tooltip damage entry

--- GlobalInstancedItems.cs
+++ GlobalInstancedItems.cs
@@ -7,6 +7,7 @@ using Terraria.DataStructures;
 using System.Text.RegularExpressions;
 using Terraria.ID;
 using System;
+using System.Linq;
 
 namespace RandomStats
 {
@@ -90,23 +91,32 @@ namespace RandomStats
         {
             if (item.damage > 0 && item.maxStack == 1)
             {
-                string derpRegex = "[0-9]+";
-                long damageAfterBonusStats = Int64.Parse(Regex.Match(tooltips[1].Text, derpRegex).Value);
-                string damageNumberRegex = "[0-9]+";
-                long damageBonusThroughEquip = (damageAfterBonusStats - ((int)(item.OriginalDamage * (float)randomStat)));
-                string newText = Regex.Replace(tooltips[1].Text, damageNumberRegex, 
-                    (randomStat == 0 ? "" : (int)(item.OriginalDamage * (float)randomStat) + " ") 
-                        + "[" + (int)(item.OriginalDamage * (double)(rngMinValue / 100.0)) + "-" + (int)(item.OriginalDamage * (double)(rngMaxValue / 100.0)) + "]" 
-                            +(damageBonusThroughEquip != 0 && randomStat != 0 ? ((damageBonusThroughEquip>0)? "+[c/86FF70:" + Math.Abs(damageBonusThroughEquip) + "]": "-[c/FC3232:" + Math.Abs(damageBonusThroughEquip) + "]")  : ""));
-                tooltips[1].Text = newText;
+                TooltipLine tt = tooltips.FirstOrDefault(x => x.Name == "Damage" && x.Mod == "Terraria");
+                if (tt != null)
+                {
+                    string derpRegex = "[0-9]+";
+                    long damageAfterBonusStats = Int64.Parse(Regex.Match(tt.Text, derpRegex).Value);
+                    string damageNumberRegex = "[0-9]+";
+                    long damageBonusThroughEquip = (damageAfterBonusStats - ((int)(item.OriginalDamage * (float)randomStat)));
+                    string newText = Regex.Replace(tt.Text, damageNumberRegex,
+                        (randomStat == 0 ? "" : (int)(item.OriginalDamage * (float)randomStat) + " ")
+                            + "[" + (int)(item.OriginalDamage * (double)(rngMinValue / 100.0)) + "-" + (int)(item.OriginalDamage * (double)(rngMaxValue / 100.0)) + "]"
+                                + (damageBonusThroughEquip != 0 && randomStat != 0 ? ((damageBonusThroughEquip > 0) ? "+[c/86FF70:" + Math.Abs(damageBonusThroughEquip) + "]" : "-[c/FC3232:" + Math.Abs(damageBonusThroughEquip) + "]") : ""));
+                    tt.Text = newText;
+                }
             }
             else if (item.defense > 0 && item.maxStack == 1 && (item.headSlot > 0 || item.bodySlot > 0 || item.legSlot > 0))
             {
-                string defenseNumberRegex = "[0-9]+";
-                string newText = Regex.Replace(tooltips[2].Text, defenseNumberRegex, (randomStat == 0 ? "" : (int)(item.OriginalDefense * (float)randomStat) + " ") + "[" + (int)(item.OriginalDefense * (double)(rngMinValue / 100.0)) + "-" + (int)(item.OriginalDefense * (double)(rngMaxValue / 100.0)) + "]");
-                tooltips[2].Text = newText;
+                TooltipLine tt = tooltips.FirstOrDefault(x => x.Name == "Defense" && x.Mod == "Terraria");
+                if (tt != null)
+                {
+                    string defenseNumberRegex = "[0-9]+";
+                    string newText = Regex.Replace(tt.Text, defenseNumberRegex, (randomStat == 0 ? "" : (int)(item.OriginalDefense * (float)randomStat) + " ") + "[" + (int)(item.OriginalDefense * (double)(rngMinValue / 100.0)) + "-" + (int)(item.OriginalDefense * (double)(rngMaxValue / 100.0)) + "]");
+                    tt.Text = newText;
+                }
             }
         }
+
         public override void LoadData(Item item, TagCompound tag)
         {
             randomStat = tag.GetDouble("randomStat");
